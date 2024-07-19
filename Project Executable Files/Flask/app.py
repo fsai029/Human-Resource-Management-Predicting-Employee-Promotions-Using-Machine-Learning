ae9eb00860cf7ef143506d75d6bac780db7d604a
Trushant Mayekar <fsai029trusahntvmayekar@gmail.com>
2024-07-20 00:47:40 +0530

    first commit

---
+++ Project Executable Files/Flask/app.py
@@ -0,0 +1,78 @@
+import pickle,joblib
+from flask import Flask, render_template, request
+from sklearn.preprocessing import LabelEncoder
+
+model = pickle.load(open(r'C:\Users\trush\OneDrive\Desktop\project_smartbridge\training\model.pkl', 'rb'))
+
+le = LabelEncoder()
+departments = ['Sales & Marketing', 'Operations', 'Technology', 'Analytics', 'R&D', 'Procurement', 'Finance', 'HR', 'Legal']
+le.fit(departments)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/home')
+def home1():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html',department_options=departments)
+
+def preprocess_department(department):
+    # Transform the department using the pre-trained LabelEncoder
+    return le.transform([department])[0]
+
+
+@app.route('/pred', methods=['POST'])
+def pred():
+    department = preprocess_department(request.form['department'])
+    education = request.form['education']
+    if education == '1':
+        education = 1
+    elif education == '2':
+        education = 2
+    else:
+        education = 3
+    no_of_trainings = request.form['no_of_trainings']
+    age = request.form['age']
+    previous_year_rating = request.form['previous_year_rating']
+    length_of_service = request.form['length_of_service']
+    KPIs = request.form['KPIs']
+    if KPIs == '0':
+        KPIs = 0
+    else:
+        KPIs = 1
+    awards_won = request.form['awards_won']
+    if awards_won == '0':
+        awards_won = 0
+    else:
+        awards_won = 1
+    avg_training_score = request.form['avg_training_score']
+    total = [[department, education, no_of_trainings, age, float(previous_year_rating), float(length_of_service),
+              KPIs, awards_won, avg_training_score]]
+    prediction = model.predict(total)
+    if prediction == 0:
+        text = 'Sorry, you are not eligible for promotion'
+    else:
+        text = 'Great, you are eligible for promotion'
+    return render_template('submit.html', predictionText=text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
